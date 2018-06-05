39f4a417a2dc1e846e601cd2363e4e3d6d370538
Abdul Ramadan <abdul.ramadan@worldfirst.com>
2018-06-05 16:30:17 +0800

    generate the file for the ML to run

--- MLPredictor/README.md
+++ MLPredictor/README.md
@@ -1,2 +1,6 @@
 #how to call it
 python predict.py tweetsCsvPath
+
+#get the latest bitcoin values
+##todo
+https://api.coindesk.com/charts/data?data=close&startdate=2010-07-18&enddate=2018-06-05&exchanges=bpi&dev=1&index=USD
--- MLPredictor/preprocessData.py
+++ MLPredictor/preprocessData.py
@@ -7,44 +7,63 @@ Created on Tue Jun  5 11:49:38 2018
 """
 
 #Import 3 datasets (keywords, tweets, btcoinValue)
-#import pandas as pd
 def generateMainDataSet():
     import pandas as pd
-    keywords = pd.read_csv('dataplace/dictionary.csv')
-    btcvalues = pd.read_csv('dataplace/btcvalues.csv')
-    tweets = pd.read_csv('dataplace/tweets.csv')
-    dataset = pd.read_csv('dataplace/Data.csv')
-
-    print("DataLoaded")
-    return dataset
-
-
-
-keywords = pd.read_csv('dataplace/dictionary.csv').values
-btcvalues = pd.read_csv('dataplace/btcvalues.csv').values
-tweets = pd.read_csv('dataplace/tweets.csv').values
-dataset = pd.read_csv('dataplace/Data.csv')
-keywordsFreq = {}
-#truncate dataset file
-f = open('dataplace/test.csv','w')
-f.truncate() #Give your csv text here.
-#open dataset for appending
-#f = open('dataplace/test.csv','w')
-index = 0
-for word in keywords:
-    keywordsFreq[word[0]] = 0;
-    wordSep = word[0]+','
-    f.write(wordSep)
-    index +=1
-    print(wordSep)
-f.write('BTCValues\n')
-## Python will convert \n to os.linesep
-f.close()
-test = pd.read_csv('dataplace/test.csv')
-
-
-
-
-#keywordsFreq['asd'] = 10;
-#if 'asdas' in keywordsFreq:
-#    print('yes')
+    keywords = pd.read_csv('dataplace/dictionary.csv').values
+    btcData = pd.read_csv('dataplace/btcvalues.csv').values
+    tweetsData = pd.read_csv('dataplace/tweets.csv').values
+    #dataset = pd.read_csv('dataplace/Data.csv')
+    keywordsFreq = {}
+    btcValues = {}
+    
+    #initialize Keywords freq array and put in file
+    f = open('dataplace/generatedData.csv','w')
+    f.truncate()
+    index = 0
+    for word in keywords:
+        keywordsFreq[word[0]] = 0;
+        wordSep = word[0]+','
+        f.write(wordSep)
+        index +=1
+    f.write('BTCValues\n')
+    f.close()
+    test = pd.read_csv('dataplace/generatedData.csv')
+    
+    #make the btcoins values as dictionaries
+    index = 0
+    for btcvalue in btcData:
+        index +=1
+        btcValues[btcvalue[0]] = btcvalue[1]
+    
+    
+    #pick keywords and flush them in file
+    f = open('dataplace/generatedData.csv','a')
+    tmpKeywordsFreq = keywordsFreq.copy()
+    currentData = tweetsData[0][0]
+    #todo get dictionary key for current tweet
+    for tweet in tweetsData:
+        #flush into file
+        if currentData != tweet[0]:
+            currentData = tweet[0]
+            for keyword, tmpKeywordFreq in tmpKeywordsFreq.items():
+                wordSep = str(tmpKeywordFreq)+','
+                f.write(wordSep)
+            btcval = str(btcValues[currentData]) + '\n'
+            f.write(btcval)
+            tmpKeywordsFreq = keywordsFreq.copy()
+        #count frequencies
+        for word in tmpKeywordsFreq:
+            tmpKeywordsFreq[word] += tweet[1].count(word)
+        
+    f.close()
+    generatedData = pd.read_csv('dataplace/generatedData.csv')
+    return generatedData
+    
+    
+    
+    
+    
+    
+    
+    
+    
